4852cbbd13fcd783c9ade19538b6e7d2037fe12f
Xavier <xaviercastro2210@gmail.com>
2023-12-05 11:22:11 -0500

    added javascript file to do the calculations for the bmi calculator

---
+++ calculations.js
@@ -0,0 +1,5 @@
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var male = document.getElementById("m");
+var form = document.getElementById("form");
