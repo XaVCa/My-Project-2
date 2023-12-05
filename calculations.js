c9ee10a2ad3e3fabc5afe8a9787483b2eba42cb0
Xavier <xaviercastro2210@gmail.com>
2023-12-05 11:32:44 -0500

    added function called validateForm

--- calculations.js
+++ calculations.js
@@ -3,3 +3,12 @@ var height = document.getElementById("height");
 var weight = document.getElementById("weight");
 var male = document.getElementById("m");
 var form = document.getElementById("form");
+
+function validateForm(){
+    if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
+      alert("All fields are required!");
+      document.getElementById("submit").removeEventListener("click", countBmi);
+    }else{
+      countBmi();
+    }
+}
